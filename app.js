9baaf1f784619ef80d0794b7208664df472263bb
adTest <piyawat.kunama@gmail.com>
2022-02-14 17:37:11 +0700

    change port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3800;
 
 app.get("/", (req, res) => {
 	res.sendFile("/test.html", { root: __dirname });
