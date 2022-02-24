ddd66a9d8d5d522e19fdc52e5a802045b8679411
adTest <piyawat.kunama@gmail.com>
2022-02-24 15:51:57 +0700

    fix impression manual ads

--- manual-render-ads.js
+++ manual-render-ads.js
@@ -1,18 +1,19 @@
 generateAds = async () => {
-	const elements = document.getElementsByClassName("AFbrother");
-	const webKey = elements[0].getAttribute("data-web-key");
+	const AFelements = document.getElementsByClassName("AFbrother");
+	const webKey = AFelements[0].getAttribute("data-web-key");
 
 	const adKeys = [];
-	for (let i = 0; i < elements.length; i++) {
-		adKeys.push(elements[i].getAttribute("data-creative-web-key"));
+	for (let i = 0; i < AFelements.length; i++) {
+		const adKey = AFelements[i].getAttribute("data-creative-web-key");
+		if (!adKeys.includes(adKey)) {
+			adKeys.push(adKey);
+		}
 	}
 
 	const headers = new Headers();
 
 	headers.append("Access-Control-Allow-Origin", "*");
 	headers.append("Content-Type", "application/json");
-
-	console.log(webKey, adKeys);
 
 	const body = JSON.stringify({
 		webKey,
@@ -24,21 +25,27 @@ generateAds = async () => {
 		body,
 		redirect: "follow",
 	};
-
 	const response = await fetch(
 		"http://localhost:3001/creatives/render_ads",
 		requestOptions
 	);
 	const adsData = await response.json();
+	console.log(adsData);
+
 	const renderAdKeys = [];
 	for (let i = 0; i < adsData.length; i++) {
+		renderAdKeys.push(adsData[i].key);
+		let adStyle;
+
 		renderAdKeys.push(adsData[i].key);
 		// Ins
 		const insElem = document.querySelector(
 			`ins[data-creative-web-key='${adsData[i].adKey}']`
 		);
+
 		insElem.id = `tagIns${i}`;
-		const adStyle = insElem.getAttribute("style");
+
+		adStyle = insElem.getAttribute("style");
 
 		// div
 		const divElem = document.createElement("div");
@@ -46,10 +53,43 @@ generateAds = async () => {
 		if (adsData[i].ad_format === "BANNER") {
 			if (adsData[i].banner_format === "STICKY") {
 				divElem.style =
-					"display: block !important;width: 100% !important;bottom: 0px !important;margin: 0 !important;clear: none !important;float: none !important;padding: 0px !important;position: fixed !important;visibility: visible !important;z-i: 999999999 !important;text-align: center !important;";
+					"display: block !important;width: 100% !important;bottom: 0px !important;margin: 0 !important;clear: none !important;float: none !important;padding: 0px !important;position: fixed !important;visibility: visible !important;z-index: 999999999 !important;text-align: center !important;";
+				document.getElementById(`tagIns${i}`).appendChild(divElem);
+			} else if (adsData[i].banner_format === "HEADER") {
+				divElem.style = `display: block;margin: 0px;width: 100% !important;bottom: 0px;float: none !important;left: 0px;margin: 0px !important;max-height: none !important;max-width: none !important;opacity: 1;overflow: visible !important;text-align: center  !important;`;
+				document.body.insertBefore(divElem, document.body.firstChild);
+			} else if (adsData[i].banner_format === "SLIDE") {
+				const parentNode = insElem.parentNode;
+				parentNode.style.position = "relative";
+				document.getElementById(`tagIns${i}`).appendChild(divElem);
+			} else {
+				document.getElementById(`tagIns${i}`).appendChild(divElem);
+				adStyle = `display: inline-block;${adStyle}`;
 			}
+		} else {
+			document.getElementById(`tagIns${i}`).appendChild(divElem);
+			adStyle = `display: inline-block;${adStyle}`;
 		}
-		document.getElementById(`tagIns${i}`).appendChild(divElem);
+
+		if (adsData[i].ad_format === "POP") {
+			// Get session
+			let pop_up_session = sessionStorage.getItem("afbrother_pop");
+			if (!pop_up_session) {
+				var element = document.createElement("a");
+				element.id = "popup_ads";
+				element.setAttribute("href", adsData[i].url);
+
+				window.open(element, "", "width=800 height=500");
+
+				// Set session after show pop up
+				sessionStorage.setItem("afbrother_pop", "pop_up_ads_session");
+			}
+			// Romove session after 30 minute
+			setTimeout(() => {
+				sessionStorage.removeItem("afbrother_pop");
+			}, 18000000);
+		}
+
 		// A
 		const AElem = document.createElement("a");
 		AElem.id = `tagA${i}`;
@@ -85,22 +125,26 @@ generateAds = async () => {
 
 		document.getElementById(`tagDivAd${i}`).appendChild(ImgClose);
 	}
-	console.log(renderAdKeys);
-	const renderAdBody = JSON.stringify({
-		webKey,
-		adKeys: renderAdKeys,
-	});
-	const requestRenderAdOptions = {
-		method: "POST",
-		headers,
-		body: renderAdBody,
-		redirect: "follow",
-	};
+	renderAdKeys.forEach(async (renderAdKey) => {
+		const renderAdBody = JSON.stringify({
+			webKey,
+			adKey: renderAdKey,
+		});
+		const requestRenderAdOptions = {
+			method: "POST",
+			headers,
+			body: renderAdBody,
+			redirect: "follow",
+		};
 
-	await fetch(
-		"http://localhost:3001/creatives/impression_ads",
-		requestRenderAdOptions
-	);
+		await fetch(
+			"http://localhost:3001/creatives/impression_ads",
+			requestRenderAdOptions
+		);
+	});
 };
 
-generateAds();
+if (typeof window.AFAdsScript === "undefined") {
+	window.AFAdsScript = true;
+	generateAds();
+}
